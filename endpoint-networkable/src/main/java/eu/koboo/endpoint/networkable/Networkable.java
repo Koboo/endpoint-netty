7db0fde6815e614606a55e919e5142e1a8170656
Koboo <14143908+Koboo@users.noreply.github.com>
2021-06-29 11:03:14 +0200

    Refactor default methods and add generic usage

--- endpoint-networkable/src/main/java/eu/koboo/endpoint/networkable/Networkable.java
+++ endpoint-networkable/src/main/java/eu/koboo/endpoint/networkable/Networkable.java
@@ -2,10 +2,88 @@ package eu.koboo.endpoint.networkable;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
+@SuppressWarnings("all")
 public interface Networkable {
 
     void readStream(DataInputStream input) throws Exception;
 
     void writeStream(DataOutputStream output) throws Exception;
+
+    default <Prim> void write(DataOutputStream outputStream, Prim object) {
+        try {
+            if(object instanceof Boolean) {
+                outputStream.writeBoolean((Boolean) object);
+            } else if(object instanceof Byte) {
+                outputStream.writeByte((Byte) object);
+            } else if(object instanceof Character) {
+                outputStream.writeChar((Character) object);
+            } else if(object instanceof Long) {
+                outputStream.writeLong((Long) object);
+            } else if(object instanceof Float) {
+                outputStream.writeFloat((Float) object);
+            } else if(object instanceof Short) {
+                outputStream.writeShort((Short) object);
+            } else if(object instanceof Double) {
+                outputStream.writeDouble((Double) object);
+            } else if(object instanceof String) {
+                outputStream.writeUTF((String) object);
+            } else if(object instanceof Integer) {
+                outputStream.writeInt((Integer) object);
+            } else {
+                throw new IllegalStateException("No primitive found! Custom object?");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    default <Prim> Prim read(DataInputStream inputStream, Class<Prim> primClass) {
+        try {
+            Primitive primitive = Primitive.valueOf(primClass.getSimpleName());
+
+            switch (primitive) {
+                case BOOLEAN:
+                    return (Prim) Boolean.valueOf(inputStream.readBoolean());
+                case BYTE:
+                    return (Prim) Byte.valueOf(inputStream.readByte());
+                case CHAR:
+                    return (Prim) Character.valueOf(inputStream.readChar());
+                case LONG:
+                    return (Prim) Long.valueOf(inputStream.readLong());
+                case FLOAT:
+                    return (Prim) Float.valueOf(inputStream.readFloat());
+                case SHORT:
+                    return (Prim) Short.valueOf(inputStream.readShort());
+                case DOUBLE:
+                    return (Prim) Double.valueOf(inputStream.readDouble());
+                case STRING:
+                    return (Prim) inputStream.readUTF();
+                case INTEGER:
+                    return (Prim) Integer.valueOf(inputStream.readInt());
+                default:
+                    throw new IllegalStateException("No primitive found! Custom object?");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    default byte[] readArray(DataInputStream input) throws Exception {
+        int length = input.readInt();
+        byte[] buffer = new byte[length];
+        input.read(buffer);
+        return buffer;
+    }
+
+    default void writeArray(DataOutputStream output, byte[] buffer) throws Exception {
+        int length = buffer.length;
+        output.writeInt(length);
+        output.write(buffer);
+    }
 }
