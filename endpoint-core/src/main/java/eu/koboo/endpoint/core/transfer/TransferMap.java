a833617b5aa0c7d4de8740c9535d979e946a4690
Koboo <14143908+Koboo@users.noreply.github.com>
2021-07-02 09:22:47 +0200

    Optimize TransferMapPacket en/decoding

--- endpoint-core/src/main/java/eu/koboo/endpoint/core/transfer/TransferMap.java
+++ endpoint-core/src/main/java/eu/koboo/endpoint/core/transfer/TransferMap.java
@@ -1,9 +1,5 @@
 package eu.koboo.endpoint.core.transfer;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -41,51 +37,4 @@ public class TransferMap extends ConcurrentHashMap<String, Object> {
         super.put(key, value);
         return this;
     }
-
-    public static byte[] encode(TransferMap transferMap) {
-        try {
-            if(transferMap != null) {
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                DataOutputStream outputStream = new DataOutputStream(buffer);
-                int keyLength = transferMap.keySet().size();
-                outputStream.writeInt(keyLength);
-                for (Entry<String, Object> entry : transferMap.entrySet()) {
-                    String key = entry.getKey();
-                    Object object = entry.getValue();
-                    Primitive primitive = Primitive.of(object);
-                    if (primitive == null) {
-                        throw new IllegalArgumentException("Value " + object.getClass().getName() + " is not a primitive type!");
-                    }
-                    outputStream.writeUTF(primitive.name());
-                    outputStream.writeUTF(key);
-                    Primitive.writeDynamic(outputStream, object);
-                }
-                buffer.close();
-                return buffer.toByteArray();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new byte[0];
-    }
-
-    public static TransferMap decode(byte[] bytes) {
-        TransferMap transferMap = new TransferMap();
-        try {
-            ByteArrayInputStream buffer = new ByteArrayInputStream(bytes);
-            DataInputStream inputStream = new DataInputStream(buffer);
-            int keyLength = inputStream.readInt();
-            for(int i = 0; i < keyLength; i++) {
-                Primitive primitive = Primitive.valueOf(inputStream.readUTF());
-                String key = inputStream.readUTF();
-                Object object = Primitive.readDynamic(inputStream, primitive);
-                transferMap.put(key, object);
-            }
-            return transferMap;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new TransferMap();
-    }
-
 }
--- endpoint-core/src/main/java/eu/koboo/endpoint/core/transfer/TransferMapPacket.java
+++ endpoint-core/src/main/java/eu/koboo/endpoint/core/transfer/TransferMapPacket.java
@@ -4,6 +4,8 @@ import eu.koboo.endpoint.core.codec.EndpointPacket;
 import eu.koboo.endpoint.core.util.BufUtils;
 import io.netty.buffer.ByteBuf;
 
+import java.util.Map;
+
 public class TransferMapPacket implements EndpointPacket {
 
     private TransferMap transferMap;
@@ -19,13 +21,30 @@ public class TransferMapPacket implements EndpointPacket {
 
     @Override
     public void read(ByteBuf byteBuf) {
-        byte[] encoded = BufUtils.readArray(byteBuf);
-        setTransferMap(TransferMap.decode(encoded));
+        transferMap = new TransferMap();
+        int keyLength = BufUtils.readVarInt(byteBuf);
+        for(int i = 0; i < keyLength; i++) {
+            Primitive primitive = Primitive.valueOf(BufUtils.readString(byteBuf));
+            String key = BufUtils.readString(byteBuf);
+            Object object = PrimitiveUtils.readByteBuf(byteBuf, primitive);
+            transferMap.put(key, object);
+        }
     }
 
     @Override
     public void write(ByteBuf byteBuf) {
-        byte[] encoded = TransferMap.encode(getTransferMap());
-        BufUtils.writeArray(encoded, byteBuf);
+        int keyLength = transferMap.keySet().size();
+        BufUtils.writeVarInt(keyLength, byteBuf);
+        for (Map.Entry<String, Object> entry : transferMap.entrySet()) {
+            String key = entry.getKey();
+            Object object = entry.getValue();
+            Primitive primitive = Primitive.of(object);
+            if (primitive == null) {
+                throw new IllegalArgumentException("Value " + object.getClass().getName() + " is not a primitive type!");
+            }
+            BufUtils.writeString(primitive.name(), byteBuf);
+            BufUtils.writeString(key, byteBuf);
+            PrimitiveUtils.writeByteBuf(byteBuf, object);
+        }
     }
 }
